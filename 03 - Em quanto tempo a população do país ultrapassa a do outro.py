cf4616fad71677bb02e843ee09aa9df425a4f36c
moacirescorcio <moacirescorcio@gmail.com>
2021-11-19 16:27:04 -0300

    Update 03 - Em quanto tempo a população do país ultrapassa a do outro.py

--- 03 - Em quanto tempo a população do país ultrapassa a do outro.py
+++ 03 - Em quanto tempo a população do país ultrapassa a do outro.py
@@ -1,11 +1,19 @@
-populacao_a = int(input())
-populacao_b = int(input())
+populacao_a = int(input('População do país A: '))
+populacao_b = int(input('População do país B: '))
 
 ano = 0
+if populacao_a > populacao_b:
+    while populacao_a > populacao_b:
+        populacao_a = (populacao_a * 0.02) + populacao_a
+        populacao_b = (populacao_b * 0.03) + populacao_b
+        ano += 1
 
-while populacao_a > populacao_b:
-    populacao_a = (populacao_a * 0.02) + populacao_a
-    populacao_b = (populacao_b * 0.03) + populacao_b
-    ano += 1
+    print(f'Levará {ano} anos para a população do país B ultrapassar a poplução do país A!')
 
-print(ano)
+else:
+    while populacao_b > populacao_a:
+        populacao_a = (populacao_a * 0.03) + populacao_a
+        populacao_b = (populacao_b * 0.02) + populacao_b
+        ano += 1
+
+    print(f'Levará {ano} anos para a população do país A ultrapassar a população do país B!')
